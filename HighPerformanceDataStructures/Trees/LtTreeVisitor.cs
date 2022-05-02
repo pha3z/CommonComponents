6583decb309e3d43169b2046ca2429463d7f9f99
James <pha3zme@gmail.com>
2022-05-01 20:52:28 -0500

    namespace correction

--- HighPerformanceDataStructures/Trees/LtTreeVisitor.cs
+++ HighPerformanceDataStructures/Trees/LtTreeVisitor.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Faeric.Layout.HighPerformanceDataStructures
+namespace Faeric.HighPerformanceDataStructures
 {
     public delegate void ActionRef<T>(ref T item);
 
